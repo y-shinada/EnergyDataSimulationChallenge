04c5237f67eb3b59ea9c5ef18589c3e695d6ece1
y-shinada <ys09gm@gmail.com>
2019-01-22 19:54:11 +0900

    remove unused comment

--- challenge3/webapp/y-shinada/config/deploy/staging.rb
+++ challenge3/webapp/y-shinada/config/deploy/staging.rb
@@ -1,12 +1,3 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
 set :user, 'y-shinada'
 set :rails_env, 'staging'
 
@@ -41,8 +32,6 @@ set :nginx_server_names, [
 ]
 set :nginx_server_name, fetch(:nginx_server_names).join(' ')
 set :nginx_socket_flags, fetch(:nginx_flags)
-# set :nginx_ssl_certificate, "/etc/ssl/certs/#{fetch(:nginx_config_name)}.crt"
-# set :nginx_ssl_certificate_key, "/etc/ssl/private/#{fetch(:nginx_config_name)}.key"
 set :nginx_use_ssl, false
 
 server "54.178.170.155", user: fetch(:user), roles: %w{app db web}, ssh_options: {
@@ -50,55 +39,3 @@ server "54.178.170.155", user: fetch(:user), roles: %w{app db web}, ssh_options:
   forward_agent: true,
   keys: ['config/certs/hello-energy-pem.pem'],
 }
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
